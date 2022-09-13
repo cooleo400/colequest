c1543bb88720613b5cf6475710cde939aa09b7f8
cooleo400 <cole.rotante@animoto.com>
2022-09-13 11:52:31 -0400

    added sweet little girl and if i still had you videos

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -42,29 +42,20 @@ class Media extends PureComponent {
     const wayOverYonderVideo = <Video youtubeId="Yw5tZtpiYlI" />;
     const bitcoinVideo = <Video youtubeId="k8BI1KWwiTc" />;
     const ostrichVideo = <Video youtubeId="WZ_macH8E1w" />;
+    const sweetVideo = <Video youtubeId="NdZ65rBdsWs" />;
+    const ifIStillVideo = <Video youtubeId="3XDlPrSyzLk" />;
     const bandAtConveyorPhoto = (
       <a href="#" onClick={this.openModalImageOne} >
         <img src={pressPhotoOne} alt="Press Photo 1" className="display-block" />
       </a>
     );
-    // const oklahomaHeadshotsPhoto = (
-    //   <a href="#" onClick={this.openModalImageTwo}>
-    //     <img src={pressPhotoTwo} alt="Press Photo 2" className="display-block" />
-    //   </a>
-    // );
-    // const bitcoinGamblerSoundCloud = (
-    //   <iframe width="100%" height="166" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/667625246&color=%23c82c2f&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false"></iframe>
-    // );
-    // const standingMountainSoundCloud = (
-    //   <iframe width="100%" height="166" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/617323221&color=%23c82c2f&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false"></iframe>
-    // );
-
     return (
       <div>
-        <h4 className="highlight text-center">NEW SINGLES!</h4>
+        <h4 className="highlight text-center">SELF [EN]TITLED</h4>
+        <Columns leftColumnChildren={sweetVideo} rightColumnChildren={ifIStillVideo} marginBottom="small" />
         <Columns leftColumnChildren={sevenElevenVideo} rightColumnChildren={ostrichVideo} marginBottom="small" />
         <Columns leftColumnChildren={wayOverYonderVideo} rightColumnChildren={bitcoinVideo} marginBottom="small" />
-        <h4 className="text-center">GREAT OLDIES</h4>
+        <h4 className="text-center">LIVE PERFORMANCES</h4>
         <Columns leftColumnChildren={doReMiVideo} rightColumnChildren={themeTimeVideo} marginBottom="small" />
         <Columns leftColumnChildren={ostrichTherapyVideo} rightColumnChildren={waxCyclinderVideo} marginBottom="small" />
         <Columns centerColumnChildren={bandAtConveyorPhoto} marginBottom="small"/>
